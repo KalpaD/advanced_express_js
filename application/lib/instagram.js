d7c735e09c5e083c3b3fbdd59b20fca477b31e9a
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2017-05-14 14:09:33 +1000

    Adding test cases to IG api , used spy features from sinon js.

--- application/lib/instagram.js
+++ application/lib/instagram.js
@@ -28,7 +28,9 @@ ig.prototype.user_self_media_recent = function(callback) {
 }
 
 ig.prototype.user_self_media_recent_with_cache = function(callback) {
+    console.log('Entering user_self_media_recent_with_cache');
     var data = this.fetch_from_cache();
+    console.log('Data from cache : '+ data);
     if (data) {
         return callback(null, data.medias, data.limit);
     }
@@ -36,6 +38,7 @@ ig.prototype.user_self_media_recent_with_cache = function(callback) {
 }
 
 ig.prototype.fetch_from_cache = function() {
+    console.log('Entering fetch_from_cache');
     if (this.cache.medias && (new Date).getTime() < this.cache.timeout) {
         return {"medias": this.cache.medias, "limit": this.cache.limit};
     }
--- application/test/instagram.js
+++ application/test/instagram.js
@@ -1,5 +1,6 @@
 var expect = require('expect.js');
 var IG = require('../lib/instagram');
+var sinon = require('sinon');
 
 // Create IG instance
 var ig_api = new IG('access_token');
@@ -9,6 +10,14 @@ ig_api.user_self_media_recent_with_cache = (callback) => {
     return callback(null, [{id: '001'}, {id: '002'}, {id: '003'}], 0, 2, 10);
 }
 
+beforeEach(() => {
+    this.sinon = sinon.sandbox.create();
+});
+
+afterEach(() => {
+    this.sinon.restore();
+});
+
 describe('The credentials', () => {
     it('Should return the correct access token', () => {
         expect(ig_api.get_access_token()).to.be('access_token');
@@ -16,9 +25,19 @@ describe('The credentials', () => {
 
     // Test success for the user_self_media_recent_with_cache()
     it('Should return data from the user_self_media_recent_with_cache', (done)=> {
-        ig_api.user_self_media_recent_with_cache((err, medias, pagination, remaining, limit)=> {
+        ig_api.user_self_media_recent_with_cache((err, medias, pagination, remaining, limit) => {
             expect(err).to.be(null);
             expect(medias.length).to.be.greaterThan(0);
+            done();
+        });
+    });
+
+    it('Should call fetch_from_cache when loading via cache', (done)=> {
+        // Set up the spy using sinon
+        this.sinon.spy(ig_api, 'fetch_from_cache');
+
+        ig_api.user_self_media_recent_with_cache((err, medias, pagination, remaining, limit) => {
+            expect(ig_api.fetch_from_cache.callCount).to.be(0);
             done();
         });
     });
